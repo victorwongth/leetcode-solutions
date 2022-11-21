02ebf4466078b2f701bf6d36a357d1eafddd62c4
Victor Wong <54518071+victorwongth@users.noreply.github.com>
2022-11-21 20:54:12 +0000

    Create 128-longest-consecutive-sequence.py

---
+++ 128-longest-consecutive-sequence.py
@@ -0,0 +1,35 @@
+class Solution:
+    def __init__(self) -> None:
+        self.longest: int = 0
+        self.seen: Set[int] = set()
+
+    def longestConsecutive(self, nums: List[int]) -> int:
+        self.nums: Set[int] = set(nums)
+        if len(nums) == 0:
+            return 0
+
+        for num in self.nums:
+            if num not in self.seen:
+                self.seen.add(num)
+                length = 1
+                length += self._checkUpperAdjacent(num + 1)
+                length += self._checkLowerAdjacent(num - 1)
+                if length > self.longest:
+                    self.longest = length
+        return self.longest
+
+    def _checkUpperAdjacent(self, num: int) -> int: 
+        if num in self.seen:
+            return 0
+        if num not in self.nums:
+            return 0
+        self.seen.add(num)
+        return 1 + self._checkUpperAdjacent(num + 1)
+        
+    def _checkLowerAdjacent(self, num: int) -> int:
+        if num in self.seen:
+            return 0
+        if num not in self.nums:
+            return 0
+        self.seen.add(num)
+        return 1 + self._checkLowerAdjacent(num - 1)
